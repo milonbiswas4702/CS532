59740674ac8f0cbc8bba5694c5d5a35625454abe
Milon Biswas <84205020+milonbiswas4702@users.noreply.github.com>
2024-01-15 11:51:04 -0600

    lab01.c

--- Lab/demo.c
+++ Lab/demo.c
@@ -1,7 +1,17 @@
-#include<stdio.h>
-
-int main()
+#include <stdio.h>
+int main() 
 {
-    printf("hello ");
+    int given_number;
+    printf("Enter number: ");
+    scanf("%d", &given_number);
+
+    if (given_number % 2 == 0) 
+    {
+        printf("The number is an even number\n");
+    } 
+    else 
+    {
+        printf("The number is an odd number\n");
+    }
     return 0;
 }
